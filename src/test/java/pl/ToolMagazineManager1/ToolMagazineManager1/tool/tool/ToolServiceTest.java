b51b3bc4a33e0572556d12510be5af04e5236c00
GrabolMariusz <grabolmm@gmail.com>
2021-12-22 11:09:01 +0100

    writing tests continue 1

--- src/test/java/pl/ToolMagazineManager1/ToolMagazineManager1/tool/tool/ToolServiceTest.java
+++ src/test/java/pl/ToolMagazineManager1/ToolMagazineManager1/tool/tool/ToolServiceTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.ToolMagazineManager1.ToolMagazineManager1.user.User;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,14 +58,72 @@ class ToolServiceTest {
 
     @Test
     void deleteTool() {
+        //given
+        long id = 1;
+        Tool tool = new Tool();
+        given(toolRepository.findById(id)).willReturn(Optional.of(tool));
+
+        //when
+        underTest.deleteTool(id);
+
+        //then
+        verify(toolRepository).deleteById(id);
     }
 
     @Test
-    void updateTool() {
+    void willThrownWhenDeleteToolNotFound() {
+        //given
+        long id = 1;
+        Tool tool = new Tool();
+        given(toolRepository.findById(id)).willReturn(Optional.empty());
+
+        //when
+        //then
+
+        assertThatThrownBy(() -> underTest.deleteTool(id)).isInstanceOf(IllegalStateException.class).
+                hasMessageContaining("tool with id " + id + " does not exist");
+        verify(toolRepository, never()).deleteById(any());
     }
 
     @Test
-    void buyTool() {
+    void canUpdateTool() {
+        //given
+        long id = 1;
+        GroupName groupName = GroupName.DRILL_INDEXABLE_INSERTS;
+        String diameter = "";
+        String company = "";
+        String companyCode = "";
+        int minMagazineQuantity = 1;
+        Tool tool = new Tool(groupName,
+                "10",
+                "ceratizit",
+                "12345",
+                5);
+        given(toolRepository.findById(id)).willReturn(Optional.of(tool));
+
+        //when
+        underTest.updateTool(id, groupName, diameter, company, companyCode);
+
+        //then
+        assertThat(tool.getGroupName()).isEqualTo(groupName);
+        assertThat(tool.getDiameter()).isEqualTo(diameter);
+        assertThat(tool.getCompany()).isEqualTo(company);
+        assertThat(tool.getCompanyCode()).isEqualTo(companyCode);
+    }
+
+    @Test
+    void canBuyTool() {
+        //given
+        long toolId = 1;
+        int boughtQuantity = 10;
+        Tool tool = new Tool();
+        given(toolRepository.findById(toolId)).willReturn(Optional.of(tool));
+
+        //when
+        underTest.buyTool(toolId, boughtQuantity);
+
+        //then
+        assertThat(tool.getMagazineQuantity()).isEqualTo(10);
     }
 
     @Test
