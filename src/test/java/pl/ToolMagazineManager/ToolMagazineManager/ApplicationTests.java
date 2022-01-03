42f49a8b9e918f7852b70f60c22c85fc69aa9f88
grabolmm <90252375+grabolmm@users.noreply.github.com>
2022-01-03 12:43:02 +0100

    Update ApplicationTests.java

--- src/test/java/pl/ToolMagazineManager/ToolMagazineManager/ApplicationTests.java
+++ src/test/java/pl/ToolMagazineManager/ToolMagazineManager/ApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.ToolMagazineManager1.ToolMagazineManager1;
+package pl.ToolMagazineManager.ToolMagazineManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
